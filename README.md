a0cd7bd21e352abc003a571eea0c9aad35de5a3c
Helena Penha <helena.penha@grupozap.com>
2019-12-02 22:22:46 -0300

    first configs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # simpleRestApiJavaEE
+
+A simple example of a Rest API made with Java EE
---
+++ src/main/java/br/com/lena/App.java
@@ -0,0 +1,7 @@
+package br.com.lena;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
